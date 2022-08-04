aca1b49184c83351fc9935e4b0ff95e1e90da81e
Jakub Przepióra <42652452+jakub-przepiora@users.noreply.github.com>
2022-08-04 19:01:21 +0200

    Includes css & js
    install webpack
    include bootstrap
    include js

--- assets/app.js
+++ assets/app.js
@@ -7,6 +7,20 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+import '/node_modules/bootstrap/dist/css/bootstrap.css';
 
 // start the Stimulus application
-import './bootstrap';
+// import 'bootstrap';
+
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
+
+// or you can include specific pieces
+// require('bootstrap/js/dist/tooltip');
+// require('bootstrap/js/dist/popover');
+
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
